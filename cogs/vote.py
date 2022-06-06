e12b25fd43bb213ef8f09c4b4e968ab3cb0be1da
Swagnar <derkarus@gmail.com>
2022-06-06 11:07:48 +0200

    fixed bug with vote that took only one word as argument, changed arg to *args

--- cogs/vote.py
+++ cogs/vote.py
@@ -11,23 +11,27 @@ class Vote(commands.Cog):
         print("Vote cog is loaded")
 
     @commands.command()
-    async def vote(self, ctx, text):
+    async def vote(self, ctx, *args):
 
         # TODO: change to proper channel ID
-        if(ctx.channel.id != 820700681375449092):
+        if(ctx.channel.id != 820700681375449092 and ctx.channel.id != 982536651890896966):
             await ctx.send("Głosować można tylko na kanale #głosowania")
         else:
+
+            # delete user message containing command content
+            await ctx.channel.purge(limit=1)
+
             # get the command author
             author = ctx.author
 
             # create emoji table, 0 - ✅, 1 - ❌
             emojis = ["\N{WHITE HEAVY CHECK MARK}", "\N{CROSS MARK}"]
 
-            # construct the message and send it
-            content = '**'+str(author)+'**'+' głosuje o: '+text
+            # construct message and send it
+            content = '**'+str(author)+'**'+' głosuje o: *'+ " ".join(args) + "*"
             message = await ctx.send(content)
 
-            # foreach emoji in emojis table, add it as a reaction to the sent message
+            # foreach emoji in emojis table, add it as a reaction to the message
             for emoji in emojis:
                 await message.add_reaction(emoji)
 
